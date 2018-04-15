285a538bdb1582cb7f24e4eb04368c97c44a904d
lathieuhiep <khacdiepkma90@gmail.com>
2018-04-15 23:52:43 +0700

    update check class page element

--- page.php
+++ page.php
@@ -1,21 +1,28 @@
 <?php
 get_header();
 
-$iweb_check_elementor = get_post_meta( get_the_ID(), '_elementor_edit_mode', true );
+$iweb_check_elementor =   get_post_meta( get_the_ID(), '_elementor_edit_mode', true );
+
+$iweb_class_elementor =   '';
+
+if ( $iweb_check_elementor ) :
+    $iweb_class_elementor =   ' site-container-elementor';
+endif;
 
 ?>
 
-<main class="site-container">
+    <main class="site-container<?php echo esc_attr( $iweb_class_elementor ); ?>">
 
-    <?php
-    if ( $iweb_check_elementor ) :
-        get_template_part('template-parts/page/content','page-elementor');
-    else:
-        get_template_part('template-parts/page/content','page');
-    endif;
-    ?>
+        <?php
+        if ( $iweb_check_elementor ) :
+            get_template_part('template-parts/page/content','page-elementor');
+        else:
+            get_template_part('template-parts/page/content','page');
+        endif;
+        ?>
 
-</main>
+    </main>
 
-<?php get_footer(); ?>
+<?php 
 
+get_footer();
